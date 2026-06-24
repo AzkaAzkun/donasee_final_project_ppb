ef195e244d06e5c234514a0ebb28069920317f60
Azka rizqullah <azkarizqullah99@gmail.com>
2026-06-24 10:13:18 +0700

    feat: restore secure fcm configuration

---
+++ lib/services/fcm_credentials.dart
@@ -0,0 +1,13 @@
+const Map<String, String> fcmServiceAccountCredentials = {
+  "type": "service_account",
+  "project_id": "final-project-ppb-35cb4",
+  "private_key_id": "1af63662896db9ce3fc00e8a7c1d766e93eefcb1",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCvbJK7tnta9UKY\n4jMM0xSNFrYDhfx4i7GqB06/UXN08I64MtIP8Is2dxQ7ggtwoDv1E0hfXzDrKGhw\n9uZ+am+BN/1/Z2g2wT+b8rbhPzV27g07zMneFev5TP+arZBoaMW4IXho2Mj2AEsR\nFEMFVlw3pHzteLl0A3iVTUyDzkWmLWIDxph1Nc+c0yRVpIlZH8zV8o/1J6xh659W\nond7IPmMKW3DIzj5KEyq2Rc6CcN/QHvKnY5Y/DuUGo0EqdZTG6E7OKAUNVhmqzwF\neaym1ks8XTByCG8scYAbrCx+Sdcj78UQeChiCT7pSN5jz3faqMZxWv52qtdEr5M5\nMqD6hIo7AgMBAAECggEAEbxmcXgXrLCTVSW5h5hnwYQETwLi7cUh51Dp00oG5FHV\n/CWXO2lUieMsNoAFbBOnPDYA4TOcnInvVUyBLVLRhz0D8jegP7j2F2xItSlQ3H9g\n8yTXXA7XVtNEHH1j1kqRgvZPWDpixwxsioOD6yw6Ep4cy12jUmlvbZjtHX5jaFOP\nXDLsdxhhadvbxZqSciYTbuDx66e6qSI6OPMaYxRaGYZzFFC+mIN4T4eG7bgRT7RC\nuAY1wAaRgen5VnVaTM0wQvXUMSvlsLrxpvmrDjyZ3TTJYb7iT28ZY/tpQ9dsiabQ\nNugne+GanCfHc9iYhUp77mKZtSckwU/frJ2WhRv4wQKBgQDiwsqJZ4RvT2dGeS5y\nbDaLyK9fGi+fyUggwsWRb1asxjlr0jH9T/mtzgTeWQdchL5F7CgE20aoesjzwjgZ\n6DZdve80VWUbgIEuZoXKLoVOsL9rQtmYwRv5Q1bGkQxIgXotLLQdKr7AfD6HRWoj\n5mQ5Onv88y7H2YmMSrFpogvdawKBgQDGCzFZsmcZR+LNMPuczxByZC2Z69PfvtqJ\nWaBX793hmkIL4S+OSuiUs5KGGibVo9OjWfrqiOa8usI2QhvH7M736Ypcj8Bw9Kqx\n+u6NPkyAOSSCPyr4yvEKyGJ8cQns3TXtSQ3mbvYr/Jh1HU/o3EZb4bKSQJysG3Ud\nZhJxRazqcQKBgDbKMBvRGku22NJLV3CoOum9fLoy3ozyGm5oAUqppzypzJgDurZl\nC4P2QnDi/Adbv4mvjN/Xh5QPztwu8lU4KWKvEMwJUhu6CfyHIw59/RHNXQLyrRbe\nmf65SY9C3r/qIPYnVgGyIVLq8kTG/eVctpypR+oPbB83radrKMDIOauvAoGAHt2D\ntX0OgGwptt0ZedjzsUBXsxOEE+0E1j83tDzYEd8eyAcyiDS4huRtw6SzxLwhseXG\nK2NmwqdAuYAYIFKR9yVYwRBOZ8xaBh8bqru45IsDW6FIqznF025jmw1rBJV9YBvw\npKdKipR1llsZJyudc5h7Yjvbrx8gY5KS8UBwsKECgYEAvfUA4OOkbim1Du3V17VE\nqofQkvKkph2Ffq5YZQ2E6Gf1XnmzepKl5QCu1LFlYiLoJ71+1Pi9XbmbZwy5nu9+\nnzT/uM36hhgV3RtUyrAnsCkhpiBAaeHigHwonkKXKcNHnQk5JCa11x7vuQlnu8Pe\ncsujtrzIpoydEXhCG8z17X0=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@final-project-ppb-35cb4.iam.gserviceaccount.com",
+  "client_id": "105552575632384062363",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40final-project-ppb-35cb4.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
